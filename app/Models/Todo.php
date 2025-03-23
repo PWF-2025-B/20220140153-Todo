59318f0aea2d75e113dcb65bad47a715f7bc6324
Azmi <azmianshari123@gmail.com>
2025-03-23 15:36:51 +0700

    migration, database seeder, factory

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Tambahkan ini
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+    use HasFactory; // Tambahkan ini
+
     protected $fillable =  [
         'title', 
         'description', 
@@ -13,9 +16,7 @@ class Todo extends Model
     ];
 
     public function user()
-{
-    return $this->belongsTo(User::class);
+    {
+        return $this->belongsTo(User::class);
+    }
 }
-
-}
-
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Todo;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Membuat satu user khusus
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'is_admin' => true
         ]);
+
+        
+        User::factory(101)->create();
+
+        
+        Todo::factory(500)->create();
     }
 }
